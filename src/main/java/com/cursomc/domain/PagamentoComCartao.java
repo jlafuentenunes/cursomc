b5f926ded8cbf8a30a1f9632fb8d1a9e87df1de3
unknown <jlafuentenunes@gmail.com>
2018-08-04 12:07:27 +0100

    Pedido, Estado e Pagamento (Um para Um)

---
+++ src/main/java/com/cursomc/domain/PagamentoComCartao.java
@@ -0,0 +1,25 @@
+package com.cursomc.domain;
+
+import com.cursomc.domain.enums.EstadoPagamento;
+
+public class PagamentoComCartao extends Pagamento{
+	private static final long serialVersionUID = 1L;
+	
+	private Integer numeroDeParcelas;
+	
+	public PagamentoComCartao() {}
+
+	public PagamentoComCartao(Integer id, EstadoPagamento estado, Pedido pedido, Integer numerodeParcelas) {
+		super(id, estado, pedido);
+		this.numeroDeParcelas = numerodeParcelas;
+	}
+
+	public Integer getNumeroDeParcelas() {
+		return numeroDeParcelas;
+	}
+
+	public void setNumeroDeParcelas(Integer numeroDeParcelas) {
+		this.numeroDeParcelas = numeroDeParcelas;
+	}
+
+}
---
+++ src/main/java/com/cursomc/domain/PagamentoComCheque.java
@@ -0,0 +1,40 @@
+package com.cursomc.domain;
+
+import java.util.Date;
+
+import com.cursomc.domain.enums.EstadoPagamento;
+
+public class PagamentoComCheque extends Pagamento{
+	private static final long serialVersionUID = 1L;
+	
+	private Date dataVencimento;
+	private Date dataPagamento;
+	
+	public PagamentoComCheque() {
+		
+	}
+
+	public PagamentoComCheque(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
+		super(id, estado, pedido);
+		this.dataPagamento = dataPagamento;
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+	
+
+}
